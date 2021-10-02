06c105f3d52b15477bc22dc3337987077d0e7098
Climax_ <72243209+Code-Blender-7@users.noreply.github.com>
2021-10-02 17:01:51 +0600

    added script.py

---
+++ Computer VS. Computer Guessing Game/script.py
@@ -0,0 +1,20 @@
+import random
+
+# 1. Generate Answer Value from Range of 1 to 10
+guess_no = random.randint(a=1, b=10)
+attempts = 0
+while True:
+    # 2. Generate random Number based on Random Seed (1 <= x <= 100000)
+    random.seed(random.randint(a=1, b=100000))
+    choice = random.randint(a=1, b=10)
+
+    if choice <= 10:
+        if choice == guess_no:
+            attempts += 1 
+            print(f'Machine got it correct at attempt {attempts}')
+            break
+        else:
+            # 3. failure Attempt count
+            attempts += 1 
+            print(f'Attempt#{attempts} failed')
+            continue
