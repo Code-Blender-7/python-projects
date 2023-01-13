6ac33c864ae8f359ef79fa146a36d82d29beb167
Code-Blender-7 <72243209+Code-Blender-7@users.noreply.github.com>
2023-01-13 12:35:54 +0600

    Updated code for continuation scenario. Added gitignore
    According to the book, the code that I writen should do all the features that were included for the bagels project. I added docstring as well for the encapsulations.
    
    There are other minor things to do, but the code is completed. I think.

--- Bagels-Detective_Game/main.py
+++ Bagels-Detective_Game/main.py
@@ -4,30 +4,90 @@
 
 import random
 
-answer = str(random.randint(100, 999))
+
+running = True
+attempts_Limit = 3
+attempts = 1
+
+def generateAnswer():
+    """Creates new answer based on a new seed
+
+    Returns:
+        int: Answer of the game
+    """
+    random.seed(random.randint(a=1, b=100000))
+    return str(random.randint(100, 999)) # Set range from 100 to 999
+
+
+
+answer = generateAnswer()
 print(answer)
-    
+
+
 def checkValExist(user_List, answer_list):
+    """
+    Checking if the given list matches the target list
+
+    Args:
+        user_List (list): User's answer in list
+        answer_list (list): Correct answer in list
+
+    Returns:
+        int: How many user values int are not inside the answer list
+    """
+    
     NotExistValue = 0
     for i in range(3):
-        if user_input[i] == answer[i]: print("Fermi")
-        elif user_input[i] not in list(answer): NotExistValue += 1
-        elif user_input[i] != answer[i]: 
-            if user_input[i] in answer: 
+        if user_input[i] == answer_list[i]: print("Fermi")
+        elif user_input[i] not in list(answer_list): NotExistValue += 1
+        elif user_input[i] != answer_list[i]: 
+            if user_input[i] in answer_list: 
                 print("Pico")
-    return NotExistValue
                 
-while True:
+    return NotExistValue
+
+
+def continueGame():
+    """
+    Confirmation either the user wants to play or not.
+    Mutates the running variable
+    Prompts user to give an input (y/n)
+    """
+    global running, answer, attempts
+    
+    choiceContinue = input("Do you want to continue? (y/n)")
+    if choiceContinue == "y":
+        attempts = 1
+        print("new game!")
+        answer = generateAnswer()
+
+    elif choiceContinue == "n":
+        print("Thanks for playing!")
+        running = False
+
+    else: 
+        print("wrong input\nForced exit.")
+        running = False
+
+
+while running:
+
+    print(f"Attempt #{attempts}")
     user_input = input("> ")
 
-    if len(list(user_input)) > 3:
-        print("More than 3 chars are not allowed")
+    
+    if len(list(user_input)) != 3:
+        print("The Digits must be 3 characters")
         continue
 
+    elif attempts == attempts_Limit and user_input != answer:
+        print("Attempts Reached! Game over")
+        break
     elif user_input == answer:
         print("You got it!")
-        break
-    
+        continueGame()
+
     else:
-        print(checkValExist(list(user_input), list(answer)))
-        
+        NotFoundNumbers = checkValExist(list(user_input), list(answer))
+        if NotFoundNumbers == 3: print("bagels")
+        attempts += 1
