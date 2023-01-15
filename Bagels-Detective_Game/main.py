dda56ab0cf24846a09d46c336b1200b25ee8d721
Code-Blender-7 <72243209+Code-Blender-7@users.noreply.github.com>
2023-01-16 02:31:56 +0600

    updated codebase for bugs + Added comments
    Recent tests showed that the bagels project overlooks if the value is less than 100 i.e 026,099. It is fixed now.
    
    Code follows a synchronous behavior to avoid ValueErrors

--- Bagels-Detective_Game/main.py
+++ Bagels-Detective_Game/main.py
@@ -44,8 +44,8 @@ def checkValExist(user_List, answer_list):
     Checking if the given list matches the target list
 
     Args:
-        user_List (list): User's answer in list
-        answer_list (list): Correct answer in list
+        user_List (list): User's answer in list (str)
+        answer_list (list): Correct answer in list (str)
 
     Returns:
         int: How many user values int are not inside the answer list
@@ -93,24 +93,28 @@ while running: # main loop
     print(f"\nAttempt #{attempts}")
     user_input = input("> ")
 
-    
-    if len(str(user_input)) != 3:
-        print("The Digits must be 3 characters")
-        continue
-    
-    elif not user_input.isdigit():
+    # 1. detect if the value has a string letter on it
+    if not user_input.isdigit():
         print("Strings are not allowed. Try again!")
         continue
-
+    
+    # 2. detect if the value is exceeded or below parameters
+    elif not int(user_input) >= 100 or int(user_input) > 999:
+        print("Given answer is invalid to conditions")
+        continue
+    
+    # 3. Check if attempts are reached and answer is still not correct
     elif attempts == attempts_Limit and user_input != answer:
         print("Attempts Reached! Game over")
         print(f"Correct answer was {answer}\n")
         continueGame()
-    
+        
+    # 4. Check if answer is a match (correct answer)
     elif user_input == answer:
         print("You got it!")
         continueGame()
 
+    # 5. If none of the above, return the clue to the player (fermi, pico, bagels)
     else:
         NotFoundNumbers = checkValExist(user_input, answer)
         if NotFoundNumbers == 3: print("bagels")
