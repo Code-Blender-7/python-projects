b9fbe99cd11a03a4c4f5e6feb787e5b0384e427b
Code-Blender-7 <72243209+Code-Blender-7@users.noreply.github.com>
2023-01-31 20:05:29 +0600

    Updated Codebase + Deleted Old code

--- Birthday-Paradox/main2.py
+++ Birthday-Paradox/main2.py
@@ -6,28 +6,10 @@ from datetime import datetime, timedelta
 from time import sleep
 import random
 import argparse
-from argparse import RawDescriptionHelpFormatter
+from argparse import RawTextHelpFormatter
 
 
-## 1. Get how many birthdays to generate. Maximun range is 100
-def getRange():
-    """
-    Get the range of list to be generated.
-    Updates searchLimits to mutate the values
-    Returns:
-        inputAmount = Number of dates per list
-    """
-
-    while True:
-        try:
-            inputAmount = int(input("How many birthdays should be generate? (max 100): "))
-            if inputAmount > 100: print("Value out of range! (max 100)\nIf above 100 is given, it will take a long time for the calculation to be completed!\n")
-            else: break
-        except ValueError: print("Input is incorrect. Try again") 
-    return inputAmount
-
-
-## 2. Generate Random Dates
+## 1. Generate Random Dates
 def generateBirthday(totalDates):
     """
     Function to generate birthdays
@@ -47,7 +29,7 @@ def generateBirthday(totalDates):
     return date_list
     
 
-# 3. Match birthdays that are the same
+# 2. Match birthdays that are the same
 def matchingBirthdaysCount(birthdatesList):
     """
     Returns the matched dates count in the list
@@ -69,6 +51,14 @@ def matchingBirthdaysCount(birthdatesList):
 
 
 def main(searchLimits, simulationRuns):
+    """
+    Main Features execution
+
+    Args:
+        searchLimits (int): Numbers of dates in a single list
+        simulationRuns (int): Number of simulations to be ran
+    """    
+    
     
     simulationMatched = 0 # count of the simulation of the atttempts scenarios that had a match in them
     birthdatesMatched = 0 # Number of birthdays matched in a list
@@ -80,43 +70,40 @@ def main(searchLimits, simulationRuns):
     print(", ".join(storedList))
     print(f"\nThere were {len(matchingBirthdaysCount(storedList))} dates that were the same")
     
+    # 3. Run the simulation over the specified times
     print(f"\nMatching the birthdays of {searchLimits} over {simulationRuns} times")
     if searchLimits > 80: print("Note that this may take a while") 
-    
-    
-    # 4. Run the simulation over the specified times
-    print(f"Notice. Starting the simulations over {simulationRuns} times in 3 seconds. Ctrl+C to Abort if you wish.")
+    print(f"Notice. Starting the simulations over {simulationRuns} times in 3 seconds. Ctrl+C to Abort if you wish.\n")
     sleep(3)
+    print("Started...\n")
     for i in range(simulationRuns):
         currentScenario = matchingBirthdaysCount(generateBirthday(searchLimits))
         if len(currentScenario) > 0: # if there is a match
             simulationMatched += 1
             birthdatesMatched += len(currentScenario)
         if i % 10000 == 0 and i != 0: print(f"Passing {i} scenario attempts. Matched: {birthdatesMatched}")
-        
+    print(f"Passed {simulationRuns} attempts.")
+    
+    # 3.5 After simulations reports 
     print(f"""
 Out of {simulationRuns} times generating over {simulationRuns*searchLimits} random dates , {birthdatesMatched} birthday dates are the same.
 It means that out of {searchLimits} peoples, there is a {round(simulationMatched / simulationRuns * 100, 2)}% probability some may have the same birthday.
 It could be more than you think!
 """)
  
-# main()
-
-parser = argparse.ArgumentParser(description="""
-Birthday Paradox. A program to figure out the same birthday probability between two people.
-""", 
+### Command Line Arguments
+parser = argparse.ArgumentParser(description="Birthday Paradox. A program to figure out the same birthday probability between two people.", 
 epilog="""
 Original Idea: Al Sweigart al@inventwithpython.com
 Code Author: Ahnaf Tahmid Jaheen (Foxtbird)
 """, 
-formatter_class=RawDescriptionHelpFormatter) # Check docs of argparse. 
+formatter_class=RawTextHelpFormatter) # Check docs of argparse. 
 
-parser.add_argument("--dates", help="Number of dates in a single simulation [required]", type=int, metavar="int")
-parser.add_argument("--simulations", help="Total simulations to be run over [default 100,000]", default=100000, metavar="int", type=int)
-
+parser.add_argument("--dates", type=int, metavar="int", help="Number of dates in a single simulation [numbers] [max is 100]")
+parser.add_argument("--simulations", help="Total simulations to be run over [default 100,000] [optional]", default=100000, metavar="int", type=int)
 args = parser.parse_args()
 
 if args.dates:
     if args.dates < 100: main(args.dates, args.simulations)
-    else: print("Date Value cannot be above 100")
+    elif args.dates > 100: print("Date Value cannot be above 100")
 else: parser.print_help()
